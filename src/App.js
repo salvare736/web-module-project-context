24ccdfac045b201691b221ea9a58a360a5f4e054
Sergio Alvarez <salvare736@gmail.com>
2021-04-15 16:13:04 -0400

    Completed up thru task 2

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import { ProductContext } from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([
+			...cart,
+			item
+		]);
 	};
 
 	return (
